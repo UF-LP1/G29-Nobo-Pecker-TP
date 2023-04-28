d0c6a25e6ded7c81b17bec2f1f739a35fc1e0fe9
agostina nobo <agosnobo@yahoo.com>
2023-04-28 11:14:12 -0300

    _

--- Model/tipoEmpleado.h
+++ Model/tipoEmpleado.h
@@ -6,6 +6,6 @@
 #ifndef _TIPOEMPLEADO_H
 #define _TIPOEMPLEADO_H
 
-enum tipoEmpleado {farmaceutico, ortopedia, perfumeria };
+enum tipoEmpleado {farmaceutico_, ortopedia_, perfumeria_ };
 
 #endif //_TIPOEMPLEADO_H
