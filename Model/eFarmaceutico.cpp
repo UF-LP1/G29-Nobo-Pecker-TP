3261cb209e0f0bd04852d9c2d260e71512b16db5
agostina nobo <agosnobo@yahoo.com>
2023-04-29 12:03:18 -0300

    final

--- Model/eFarmaceutico.cpp
+++ Model/eFarmaceutico.cpp
@@ -37,4 +37,5 @@ bool eFarmaceutico::venderMedicamento(CLIENTE c, vector<pMedicamentos> m,vector<
 /*
             this->carrito.push_back(g[i]); //agrego la golosina en el carrito (vector de productos)
             this->cantidades.push_back(cant[i]); //agrego la cantidad de esa golosina que se lleva en el vector de cantidades
-            g[i].set_stock(g[i].get_stock() - cant[i]);*/
+            g[i].set_stock(g[i].get_stock() - cant[i]);
+*/
