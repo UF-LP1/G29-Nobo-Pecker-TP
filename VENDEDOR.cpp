fc451963d112b48c4bec1fd6d376284adb339800
agostina nobo <agosnobo@yahoo.com>
2023-05-15 18:47:46 -0300

    c/d

--- VENDEDOR.cpp
+++ VENDEDOR.cpp
@@ -1,3 +1,9 @@
 #include "VENDEDOR.h"
 
- 
+VENDEDOR::VENDEDOR(string na, string dni, string tel, string mail) : EMPLEADO(na, dni, tel, mail) {
+
+}
+
+VENDEDOR::~VENDEDOR() {
+
+}
--- VENDEDOR.h
+++ VENDEDOR.h
@@ -4,6 +4,9 @@
 
 class VENDEDOR : public EMPLEADO{
 public:
+	VENDEDOR(string na, string dni, string tel, string mail);
+	~VENDEDOR();
+
 	virtual bool vender(CLIENTE* cliente, vector<PRODUCTO> productos, vector<unsigned int>cant) = 0;
 };
 
