0f2d9705fb369c42b86bfd0a382a873994155383
Paula Pecker <paulapeckerfasce@gmail.com>
2023-05-16 17:39:02 -0300

    generar necesidad con random

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include "Model/eLimpieza.h"
 #include "Model/FARMACIA.h"
 
-
+array<necesidadCliente, 3> generarNecesidades();
 
 int main() {
 
@@ -129,3 +129,38 @@ int main() {
 
 	return 0;
 }
+
+array<necesidadCliente, 3> generarNecesidades() {
+	srand(NULL);
+	int cantidad = (rand() % 3) + 1;// me devuelve 1, 2 o 3 para ver cuantas necesidades va a tener (no puedo tener más de una obra social, etc)
+	array<necesidadCliente, 3> necesidades;
+	switch (cantidad) {
+	case 1: {
+		necesidades[0] = necesidadCliente(rand() % 5);
+		necesidades[1] = necesidades[2] = unspecified;
+		break;
+	}
+	case 2: {
+		necesidades[0] = necesidadCliente(rand() % 5);
+		if (necesidades[0] == farmaciaOS || necesidades[0] == farmaciaP || necesidades[0] == PAMI) {
+			necesidades[1] = necesidadCliente(rand() % 2 + 3);//si ya tengo una obra social agrego uno de los otros dos 
+		}
+		else do {
+			necesidades[1] = necesidadCliente(rand() % 5);
+		} while (necesidades[1] == necesidades[0]);
+		necesidades[2] = unspecified;
+		break;
+	}
+	case 3: {
+		necesidades[0] = ortopedia;
+		necesidades[1] = perfumeria;
+		necesidades[2] = necesidadCliente(rand() % 3);
+	}
+
+	}
+	return necesidades;
+}
+
+
+
+
