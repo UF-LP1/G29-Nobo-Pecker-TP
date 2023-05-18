46999c0e138ec54bdafb724e91a139b040874ff5
Paula Pecker <paulapeckerfasce@gmail.com>
2023-05-18 19:26:26 -0300

    aclaro que unspecified es en .h

--- Model/necesidadCliente.h
+++ Model/necesidadCliente.h
@@ -6,6 +6,6 @@
 #ifndef _NECESIDADCLIENTE_H
 #define _NECESIDADCLIENTE_H
 
-enum necesidadCliente { farmaciaOS, farmaciaP, PAMI, perfumeria, ortopedia, unspecified};
+enum necesidadCliente { farmaciaOS, farmaciaP, PAMI, perfumeria, ortopedia, unspecifiedNC};
 
 #endif //_NECESIDADCLIENTE_H
