0d148280233c3c40e9c778741353e20ffafd66e1
agostina nobo <agosnobo@yahoo.com>
2023-05-18 19:21:34 -0300

    receta cpp y vendedor

--- Model/RECETA.cpp
+++ Model/RECETA.cpp
@@ -9,8 +9,7 @@
   * RECETA implementation
   */
 
-RECETA::RECETA(string medico, string fecha, string diagnostico, string dniCliente, pMedicamentos medicamento) : medico(medico), fecha(fecha), diagnostico(diagnostico), dniCliente(dniCliente), pMedicamentos(medicamento) {
-
+RECETA::RECETA(string medico, string fecha, string diagnostico, string dniCliente, string medicamento) : medico(medico), fecha(fecha), diagnostico(diagnostico), dniCliente(dniCliente), medicamento(medicamento){
 }
 
 RECETA::~RECETA() {
@@ -21,6 +20,6 @@ string RECETA::get_dni() {
 	return this->dniCliente;
 }
 
-pMedicamentos RECETA::get_medicamento() {
+string RECETA::get_medicamento() {
 	return this->medicamento;
 }
--- VENDEDOR.h
+++ VENDEDOR.h
@@ -1,31 +1,38 @@
 #pragma once
 #include "Model/EMPLEADO.h"
 #include "Model/CLIENTE.h"
+#include <exception>
 
 class VENDEDOR : public EMPLEADO{
 public:
 	VENDEDOR(string na, string dni, string tel, string mail);
 	~VENDEDOR();
 
-  bool vender(CLIENTE* cliente, vector<PRODUCTO> productos, vector<unsigned int>cant);
+  void vender(CLIENTE* cliente, vector<PRODUCTO> productos, vector<unsigned int>cant);
 };
 
 inline VENDEDOR::VENDEDOR(string na, string dni, string tel, string mail) : EMPLEADO(na, dni, tel, mail) {}
 
 inline VENDEDOR::~VENDEDOR() {}
 
-inline bool VENDEDOR::vender(CLIENTE* cliente, vector<PRODUCTO> productos, vector<unsigned int>cant) {
+inline void VENDEDOR::vender(CLIENTE* cliente, vector<PRODUCTO> productos, vector<unsigned int>cant) {
 	//recorro el vector de productos y de cantidades y lo agrego al carrito del cliente
-
-	for (int i = 0; i < productos.size(); i++) {
+	int i = 0;
+	try {
+	for (i = 0; i < productos.size(); i++) {
 		if (productos[i].get_stock() >= cant[i]) {
 			cliente->carrito.push_back(productos[i]);
 			cliente->cantidades.push_back(cant[i]);
 			productos[i].set_stock(productos[i].get_stock() - cant[i]);
 		}
-		else
-			return false;
+		else {
+			throw exception();
+		}
+	}
+	}
+	catch (exception& NoHayStock) {
+		cout << "No hay stock suficiente de " << productos[i].nombre << endl;
 	}
 
-	return true;
+	return ;
 }
