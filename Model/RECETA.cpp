93ec90d86f1689a0e4cb4b5321a74b229414a045
agostina nobo <agosnobo@yahoo.com>
2023-05-18 18:53:49 -0300

    receta cpp

--- Model/RECETA.cpp
+++ Model/RECETA.cpp
@@ -9,10 +9,18 @@
   * RECETA implementation
   */
 
-RECETA::RECETA(string m, string f, string d) : medico(m), fecha(f), diagnostico(d) {
+RECETA::RECETA(string medico, string fecha, string diagnostico, string dniCliente, pMedicamentos medicamento) : medico(medico), fecha(fecha), diagnostico(diagnostico), dniCliente(dniCliente), pMedicamentos(medicamento) {
 
 }
 
 RECETA::~RECETA() {
 
+}
+
+string RECETA::get_dni() {
+	return this->dniCliente;
+}
+
+pMedicamentos RECETA::get_medicamento() {
+	return this->medicamennto;
 }
