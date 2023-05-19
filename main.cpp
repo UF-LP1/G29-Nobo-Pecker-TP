e577ed241832716187e545bcd5e6890f3cefa730
Paula Pecker <paulapeckerfasce@gmail.com>
2023-05-19 13:51:39 -0300

    interaccion usuario

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 
 int main() {
-
+	cout << "A PARTIR DE AHORA COMIENZA LA SECCION NO INTERACTIVA DE LA FARMACIA:" << endl;
 	//construyo nuestra farmacia
 	FARMACIA NoboPecker("NoboPecker");
 	NoboPecker.set_fondos(100000.0);
@@ -19,7 +19,7 @@ int main() {
 
 	//construyo los productos
 	//por default los decuentos se inicializan en 0 como los demas atributos, asi que los productos que no tienen un set descuento es porque no tienen descuento
-	pOrtopedia productoOrtopedia("vendas_azules",vendas_elasticas);
+	pOrtopedia productoOrtopedia("vendas_azules", vendas_elasticas);
 	productoOrtopedia.set_precio(1000.0);
 	productoOrtopedia.set_descuento(10);
 	productoOrtopedia.set_stock(50);
@@ -48,18 +48,18 @@ int main() {
 	ASIST_AUTOM asistenteAutomatico;
 
 	//construyo una fila de clientes cliente
-		queue<CLIENTE*>filaClientes;
-	
+	queue<CLIENTE*>filaClientes;
+
 	//genero un array de necesidades para cada cliente
 	array<necesidadCliente, 3> necesidades1 = generarNecesidades();
-	array<necesidadCliente, 3>necesidades2=generarNecesidades();
+	array<necesidadCliente, 3>necesidades2 = generarNecesidades();
 	array<necesidadCliente, 3>necesidades3 = generarNecesidades();
-	
+
 	//construyo los clientes
-	CLIENTE *cliente1 = new CLIENTE("Julieta_Sosa", "44988100", asistenteAutomatico.entregarTicket(), necesidades1, 2000.0, 15000.0, 0.0, 0.0, "11244665", "juli2002@gmail.com", false, efectivo);
+	CLIENTE* cliente1 = new CLIENTE("Julieta_Sosa", "44988100", asistenteAutomatico.entregarTicket(), necesidades1, 2000.0, 15000.0, 0.0, 0.0, "11244665", "juli2002@gmail.com", false, efectivo);
 	CLIENTE* cliente2 = new CLIENTE("Carlos_Baute", "34567823", asistenteAutomatico.entregarTicket(), necesidades2, 0.0, 0.0, 0.0, 0.0, "2235448020", "carlitosbaute@gmail.com", false, credito);
 	CLIENTE* cliente3 = new CLIENTE("Maria_Esclavitud", "12345678", asistenteAutomatico.entregarTicket(), necesidades3, 2000.0, 15000.0, 700.0, 8000.0, "223154557475", "abuela_mari@gmail.com", true, debito);
-	
+
 	//agrego los clientes a la queue
 	filaClientes.push(cliente1);
 	filaClientes.push(cliente2);
@@ -68,7 +68,7 @@ int main() {
 
 	//atiendo los clientes en la fila
 	for (int i = 0; i < tamanioFila; i++) {
-		
+
 		CLIENTE* cliente = filaClientes.front();
 
 		for (int a = 0; a < 3; a++)
@@ -118,7 +118,7 @@ int main() {
 			}
 
 		}
-		
+
 		//cuando estaba por ir a pagar vio las golosinas y la tento un chocolate y unos chupetines asi que se va a comprar un chocolate y unos chupetines
 		int quiereGolosinas = rand() % 2;
 		if (quiereGolosinas == 0) { //si me devuelven todos 0 es porque se arrepintio de comprar golosinas, que se le va a hacer
@@ -130,18 +130,19 @@ int main() {
 			cantidadesGolosinas.push_back(inventarCantidades());
 			cliente->comprarGolosinas(listaGolosinas, cantidadesGolosinas);
 		}
-		
+
 		//ahora que tiene el carrito lleno va a ir a que le cobren
 		cajero.cobrar(cliente, NoboPecker);
-		
+
 		//una vez que termino de pagar, lo saco de la fila de clientes para seguir con el proximo
-		filaClientes.pop(); 
+		filaClientes.pop();
 		//delete cliente;
 	}
 	delete cliente1;
 	delete cliente2;
 	delete cliente3;
 
+	cout << "A PARTIR DE AHORA COMIENZA LA SECCION INTERACTIVA DE LA FARMACIA <3:" << endl;
 	string nombreApellido, dni, telefono, mail;
 	cout << "Ingrese su nombre y apellido: " << endl;
 	cin >> nombreApellido;
@@ -178,13 +179,13 @@ int main() {
 				else
 					necesidadUsuario[h] = necesidadCliente(necesidadActual - 1);
 			}
-			catch(exception& SeccionRepetida){ 
+			catch (exception& SeccionRepetida) {
 				cout << "Esa seccion ya fue ingresada, por favor elija otra: " << endl;
 				h--; //para que vuelva a preguntarlo, asi siempre tengo 3 distintas
 			}
 		} while (necesidadActual < 1 || necesidadActual>6);
 	}
-	
+
 	float efectivoU, appU, debitoU, creditoU;
 	cout << "Ingrese su monto en efectivo: " << endl;
 	cin >> efectivoU;
@@ -194,26 +195,169 @@ int main() {
 	cin >> debitoU;
 	cout << "Ingrese su monto en tarjeta de credito: " << endl;
 	cin >> creditoU;
-	
+
 	int ticketU;
 	do {
-	cout << "Si desea recibir ticket fisico elija 1, si desea recibir ticket digital elija 0: " << endl;
-	cin >> ticketU;
+		cout << "Si desea recibir ticket fisico elija 1, si desea recibir ticket digital elija 0: " << endl;
+		cin >> ticketU;
 	} while (!(ticketU == 0 || ticketU == 1));
-	
+
 	int prefMetPago;
 	do {
-	cout << "Ingrese su metodo de pago de preferencia: " << endl << "1- EFECTIVO" << endl << "2- CREDITO" << endl << "3- DEBITO" << endl << "4- APP" << endl;
-	cin >> prefMetPago;
+		cout << "Ingrese su metodo de pago de preferencia: " << endl << "1- EFECTIVO" << endl << "2- CREDITO" << endl << "3- DEBITO" << endl << "4- APP" << endl;
+		cin >> prefMetPago;
 	} while (!(prefMetPago >= 1 && prefMetPago <= 4));
 
 	CLIENTE* usuario = new CLIENTE(nombreApellido, dni, asistenteAutomatico.entregarTicket(), necesidadUsuario, efectivoU, appU, debitoU, creditoU, telefono, mail, bool(ticketU), metodoPago(prefMetPago - 1));
+
+	cout << "Ahora elija los productos que desea comprar: " << endl;
+
+	int productoActual;
+	int cantidadActual;
+
 	
 
+	for (int b = 0; b < 3; b++) {
+		bool sigueQueriendoProductos = true;
 
+		vector <PRODUCTO> carritoUsuario;
+		vector <unsigned int> cantidadesUsuario;
 
+		switch (usuario->get_nec()[b]) {
+		case 3: { //perfumeria
+			
+			do {
+				cout << "Ingrese el producto a eleccion: " << endl << "1-SHAMPOO VIOLETA" << endl << "2-ESMALTE ROJO" << endl << "3-NO DESEO MAS PRODUCTOS" << endl;
+				cin >> productoActual;
+				
+				switch (productoActual) {
+				case 1: {
+					cout << "Ingrese la cantidad de ejemplares: " << endl;
+					cin >> cantidadActual;
+					if (cantidadActual > 0) {
+					carritoUsuario.push_back(productoPerfumeria1);
+					cantidadesUsuario.push_back(cantidadActual);
+					}
+					break;
+				}
+				case 2: {
+					cout << "Ingrese la cantidad de ejemplares: " << endl;
+					cin >> cantidadActual;
+					if (cantidadActual > 0) {
+					carritoUsuario.push_back(productoPerfumeria2);
+					cantidadesUsuario.push_back(cantidadActual);
+					}
+					break;
+				}
+				default:
+					break;
+				}
+			} while (productoActual!=3);
+			empleadoPerfumeria.vender(usuario, carritoUsuario, cantidadesUsuario);
+			break;
+		}
+		case 4: { //ortopedia
+			do {
+				cout << "Ingrese el producto a eleccion: " << endl << "1-VENDAS AZULES" << endl << "2-NO DESEO MAS PRODUCTOS" << endl;
+				cin >> productoActual;
+				
+				switch (productoActual) {
+				case 1: {
+					cout << "Ingrese la cantidad de ejemplares: " << endl;
+					cin >> cantidadActual;
+					if (cantidadActual > 0) {
+					carritoUsuario.push_back(productoOrtopedia);
+					cantidadesUsuario.push_back(cantidadActual);
+					}
+					break;
+				}
 
+				default:
+					break;
 
+				}
+				break;
+			} while (productoActual != 2);
+			empleadoOrtopedia.vender(usuario, carritoUsuario, cantidadesUsuario);
+		}
+		case 5: { //unspecifiedNC
+			sigueQueriendoProductos = false;
+			break;
+		}
+		default: {//alguna farmacia
+			do {
+				cout << "Ingrese el producto a eleccion: " << endl << "1-IBUPROFENO" << endl << "2-ACTRON" << endl << "3-NO DESEO MAS PRODUCTOS" << endl;
+				cin >> productoActual;
+				
+				switch (productoActual) {
+				case 1: {
+					cout << "Ingrese la cantidad de ejemplares: " << endl;
+					cin >> cantidadActual;
+					if (cantidadActual > 0) {
+					carritoUsuario.push_back(medicamento1);
+					cantidadesUsuario.push_back(cantidadActual);
+					}
+					break;
+				}
+				case 2: {
+					cout << "Ingrese la cantidad de ejemplares: " << endl;
+					cin >> cantidadActual;
+					if (cantidadActual > 0) {
+					carritoUsuario.push_back(medicamento2);
+					cantidadesUsuario.push_back(cantidadActual);
+					}
+				}
+				default:
+					break;
+				}
+			} while (productoActual!=3);
+			farmaceutico.vender(usuario, carritoUsuario, cantidadesUsuario);
+			break;
+		}
+		}
+
+		if (!sigueQueriendoProductos)
+			break;
+	}
+
+	int quiereGolosinasUsuario;
+	do {
+		cout << "Ingrese 1 si desea comprar golosinas o 0 si no: " << endl;
+		cin >> quiereGolosinasUsuario;
+
+	} while (quiereGolosinasUsuario != 0 && quiereGolosinasUsuario != 1);
+	if ((bool(quiereGolosinasUsuario))) {
+		vector <pGolosinas> golosinasUsuario;
+		vector <unsigned int> cantGolosinasU;
+		do
+		{
+		cout << "Ingrese las golosinas que desea: " << endl << "1-CHOCOLATE BLANCO" << endl << "2-CHUPETIN DE FRUTILLA" << endl<<"3- NO DESEO MAS GOLOSINAS"<<endl;
+		cin >> productoActual;
+		switch (productoActual) {
+		case 1: {
+			cout << "Ingrese la cantidad de ejemplares: " << endl;
+			cin >> cantidadActual;
+			if (cantidadActual > 0) {
+			golosinasUsuario.push_back(golosina1);
+			cantGolosinasU.push_back(cantidadActual);
+			}
+			break;
+		}
+		case 2: {
+			cout << "Ingrese la cantidad de ejemplares: " << endl;
+			cin >> cantidadActual;
+			if (cantidadActual > 0) {
+			golosinasUsuario.push_back(golosina2);
+			cantGolosinasU.push_back(cantidadActual);
+			}
+		}
+		default:
+			break;
+		}
+		} while (productoActual!=3);
+		usuario->comprarGolosinas(golosinasUsuario, cantGolosinasU);
+	}
+	cajero.cobrar(usuario, NoboPecker);
 
 	delete usuario;
 	//pero antes de cerrar la farmacia hay que limpiarla 
