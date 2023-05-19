e20f996106bba957b135df9b392ec0c13435e4d0
agostina nobo <agosnobo@yahoo.com>
2023-05-19 11:25:52 -0300

    asis autom

--- Model/ASIST_AUTOM.cpp
+++ Model/ASIST_AUTOM.cpp
@@ -22,5 +22,5 @@ ASIST_AUTOM::~ASIST_AUTOM() {
 int ASIST_AUTOM::entregarTicket() { //este metodo se encarga de incrementar la cantidad total de clientes en uno y devolver el numero anterior a ese para asignarselo al cliente nuevo
 
     this->cantclientes++;
-    return cantclientes-1 ;
+    return cantclientes ;
 }
