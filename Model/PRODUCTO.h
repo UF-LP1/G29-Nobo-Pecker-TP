d287db65a0e4518215d0e7d9228c75be40297144
Paula Pecker <paulapeckerfasce@gmail.com>
2023-05-23 18:01:00 -0300

    saco el const

--- Model/PRODUCTO.h
+++ Model/PRODUCTO.h
@@ -17,7 +17,7 @@ public:
     PRODUCTO(string n);
     ~PRODUCTO();
 
-    const string nombre;
+    string nombre;
     void set_stock(int n);
     void set_precio(float precio);
     void set_descuento(unsigned int descuento);
